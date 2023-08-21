3eb1d61b5bbd16b69481297b0b87adf19d317315
a.shiderkulov <a.shiderkulov@etence.org>
2023-08-21 13:55:46 +0600

    feat:add apps for productQueue using FIFO on tcp connect

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,58 @@
+﻿using System.Net.Sockets;
+using System.Text;
+
+namespace ConsoleApp1;
+
+public enum QualityType {
+    UnexpectedValue = 0,
+    Valid = 1,
+    Defect = 2,
+}
+internal sealed class Program
+{
+    public static void Main(string[] args)
+    {
+        var client = new TcpClient();
+        client.Connect("127.0.0.1", 8888);
+
+        var stream = client.GetStream();
+        byte[] data;
+
+        while (true)
+        {
+            Console.WriteLine("1. Камера");
+            Console.WriteLine("2. Толкатель");
+            Console.WriteLine("3. Выход");
+            Console.Write("Выберите действие: ");
+
+            var choice = int.Parse(Console.ReadLine() ?? string.Empty);
+            
+            switch (choice)
+            {
+                case 1:
+                    QualityType quality;
+                    do 
+                    {
+                        Console.Write("Годный (1) или брак (2)? ");
+                        quality = (QualityType)int.Parse(Console.ReadLine()?? string.Empty);
+                        if (quality == QualityType.UnexpectedValue) Console.WriteLine("Пожалуйста введите корректное значение");
+                        
+                    } while (quality == QualityType.UnexpectedValue);
+                    
+                    data = Encoding.ASCII.GetBytes($"CAMERA:{(int)quality}");
+                    stream.Write(data, 0, data.Length);
+                    break;
+                case 2:
+                    data = Encoding.ASCII.GetBytes("PUSHER");
+                    stream.Write(data, 0, data.Length);
+                    break;
+                case 3:
+                    stream.Close();
+                    client.Close();
+                    break;
+                defalt: throw new Exception("Не было выбрано ни одно действие");
+
+            }
+        }
+    }
+}
---
+++ ProductQueue/Program.cs
@@ -0,0 +1,65 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace ProductQueueApp
+{
+    internal sealed class Program
+    {
+        public static void Main(string[] args)
+        {
+            const int maxQueueSize = 5;
+            var server = new TcpListener(IPAddress.Any, 8888);
+            server.Start();
+
+            Console.WriteLine("Ожидание подключения...");
+
+            var client = server.AcceptTcpClient();
+            Console.WriteLine("Подключено!");
+
+            var stream = client.GetStream();
+            
+            try
+            {
+                var productQueue = new Queue<string>();
+                while (true)
+                {
+                    var data = new byte[256];
+                    var bytesRead = stream.Read(data, 0, data.Length);
+                    var message = Encoding.ASCII.GetString(data, 0, bytesRead);
+
+                    if (message.StartsWith("CAMERA:"))
+                    {
+                        var parts = message.Split(':');
+                        if (productQueue.Count < maxQueueSize)
+                        {
+                            var quality = parts[1] == "1" ? "[Г]" : "[Б]";
+                            productQueue.Enqueue(quality);
+                        }
+                    }
+                    else if (message == "PUSHER")
+                    {
+                        if (productQueue.Count > 0)
+                        {
+                            productQueue.Dequeue();
+                        }
+                    }
+
+                    Console.Clear();
+                    foreach (var product in productQueue.Reverse())
+                    {
+                        Console.Write(product);
+                    }
+                }
+
+            }
+            finally
+            {
+                stream.Close();
+                client.Close();
+                server.Stop();
+            }
+            
+        }
+    }
+}
