99c82be7c13f5fda697bea5cef40e33c9b080028
a.shiderkulov <a.shiderkulov@etence.org>
2023-08-22 12:58:22 +0600

    fix: fix problem with input wrong format text

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -24,36 +24,43 @@ internal sealed class Program
             Console.WriteLine("3. Выход");
             Console.Write("Выберите действие: ");
 
-            var choice = int.Parse(Console.ReadLine() ?? string.Empty);
-            
-            switch (choice)
+            try
             {
-                case 1:
-                    QualityType quality;
-                    do 
-                    {
-                        Console.Write("Годный (1) или брак (2)? ");
-                        quality = (QualityType)int.Parse(Console.ReadLine()?? string.Empty);
-                        if (quality != QualityType.Defect && quality != QualityType.Valid) 
-                            Console.WriteLine("Пожалуйста введите корректное значение");
+                var choice = int.Parse(Console.ReadLine() ?? string.Empty);
+            
+                switch (choice)
+                {
+                    case 1:
+                        QualityType quality;
+                        do 
+                        {
+                            Console.Write("Годный (1) или брак (2)? ");
+                            quality = (QualityType)int.Parse(Console.ReadLine()?? string.Empty);
+                            if (quality != QualityType.Defect && quality != QualityType.Valid) 
+                                Console.WriteLine("Пожалуйста введите корректное значение");
                         
-                    } while (quality != QualityType.Defect && quality != QualityType.Valid);
+                        } while (quality != QualityType.Defect && quality != QualityType.Valid);
                     
-                    data = Encoding.ASCII.GetBytes($"CAMERA:{(int)quality}");
-                    stream.Write(data, 0, data.Length);
-                    break;
-                case 2:
-                    data = Encoding.ASCII.GetBytes("PUSHER");
-                    stream.Write(data, 0, data.Length);
-                    break;
-                case 3:
-                    stream.Close();
-                    client.Close();
-                    Console.WriteLine("Вы завершили работу программы");
-                    return;
-                default: Console.WriteLine("Введите корректное значнеие");
-                continue;
+                        data = Encoding.ASCII.GetBytes($"CAMERA:{(int)quality}");
+                        stream.Write(data, 0, data.Length);
+                        break;
+                    case 2:
+                        data = Encoding.ASCII.GetBytes("PUSHER");
+                        stream.Write(data, 0, data.Length);
+                        break;
+                    case 3:
+                        stream.Close();
+                        client.Close();
+                        Console.WriteLine("Вы завершили работу программы");
+                        return;
+                    default: Console.WriteLine("Вы ввели число которое не было представлено в списке");
+                        continue;
 
+                }
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine("Введите корректное числовое значение");
             }
         }
     }
